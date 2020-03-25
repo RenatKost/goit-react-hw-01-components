81185a680a7f1f5a272bc5a0cc2c0f0cdb7d52be
RenatKost <renya.show@gmail.com>
2020-03-25 15:05:42 +0400

    Updates

---
+++ precache-manifest.2a904c5ee90220874c520518f760db75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253a74ce5314c768e66a0b4e01c1c90c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7a30f32eb221335ff417",
+    "url": "/goit-react-hw-01-components/static/css/main.e37814e8.chunk.css"
+  },
+  {
+    "revision": "52240ea9616880e5b30f",
+    "url": "/goit-react-hw-01-components/static/js/2.7859c359.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.7859c359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a30f32eb221335ff417",
+    "url": "/goit-react-hw-01-components/static/js/main.386adcb9.chunk.js"
+  },
+  {
+    "revision": "5e28ef9210cdb0f50653",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.47b9d3ea.js"
+  }
+]);
